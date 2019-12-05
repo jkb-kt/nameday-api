ef74111c6b9d37a518be4b7928fa51bd3fc8555d
Jakub <jakub@qusion.com>
2019-12-05 22:04:33 +0100

    Updated to support new API version

--- README.md
+++ README.md
@@ -13,34 +13,23 @@ npm install nameday-api
 
 ## List of supported countries
 
-When using country codes in the library you can use either **country names** or **country codes**.
+When using country codes in the library you can use **country codes**.
 
--   Country names
-    -   United States
-    -   Czech Republic
-    -   Slovakia
-    -   Poland
-    -   France
-    -   Hungary
-    -   Croatia
-    -   Sweden
-    -   Austria
-    -   Italy
-    -   Germany
-    -   Spain
--   Country codes
-    -   us
-    -   cz
-    -   sk
-    -   pl
-    -   fr
-    -   hu
-    -   hr
-    -   se
-    -   at
-    -   it
-    -   de
-    -   es
+- Country codes
+  - at
+  - dk
+  - fr
+  - it
+  - sk
+  - cz
+  - es
+  - hr
+  - pl
+  - us
+  - de
+  - fi
+  - hu
+  - se
 
 ## Usage
 
@@ -55,54 +44,30 @@ import nameday from "nameday-api";
 ##### Request name days for today / tomorrow / yesterday
 
 ```javascript
-nameday.today(); # {"day":27,"month":8,"name_us":"Caesar, Cesar ... "}
-nameday.tomorrow(); # {"day":28,"month":8,"name_us":"Agustin, August, Augusta ... "}
-nameday.yesterday(); # {"day":26,"month":8,"name_us":"Percival, Percy ... "}
+nameday.today();
+nameday.tomorrow();
+nameday.yesterday();
 ```
 
 ##### Request name days for today / tomorrow / yesterday for specific country only
 
 ```javascript
-nameday.today("sk"); # {"day":27,"month":8,"name_sk":"Silvia"}
-nameday.today("Slovakia"); # {"day":27,"month":8,"name_sk":"Silvia"}
-
-nameday.tomorrow("at"); # {"day":28,"month":8,"name_at":"Adelinde, Aline, Augustin"}
-nameday.tomorrow("Austria"); # {"day":28,"month":8,"name_at":"Adelinde, Aline, Augustin"}
-
-nameday.yesterday("de"); # {"day":26,"month":8,"name_de":"Margarita, Miriam, Patricia, Teresa"}
-nameday.yesterday("Germany"); # {"day":26,"month":8,"name_de":"Margarita, Miriam, Patricia, Teresa"}
+nameday.today("sk");
+nameday.tomorrow("at");
+nameday.yesterday("de");
 ```
 
-##### Request name days for specific date
-
-> specificDay(day: number, month: number, countryCode?: string)
+##### Request name days for specific date and for specific country
 
 ```javascript
-nameday.specificDay(21, 10); # {"day":21,"month":10,"name_us":"Celina, Celine, Nobel" ... }
-```
-
-##### Request name days for specific date and for specific country only
-
-simply add optional third string parameter `countryCode`, which must be one of the supported [country codes](https://api.abalin.net/documentation)
-
-```javascript
-nameday.specificDay(29, 3, "es"); # {"day":29,"month":3,"name_es":"Jonas, Segundo"}
-
-nameday.specificDay(2, 12, "de"); # {"day":2,"month":12,"name_de":"Bibiana, Jan, Lucius"}
-
-nameday.specificDay(22, 12, "pl"); # {"day":22,"month":1,"name_pl":"Anastazy, Dobromysł, Dorian, Marta, Wincenty"}
-
-nameday.specificDay(2, 12, "hr"); # {"day":2,"month":2,"name_hr":"Marijan"}}
+nameday.specificDay(29, 3, "es");
+nameday.specificDay(2, 12, "de");
+nameday.specificDay(22, 12, "pl");
+nameday.specificDay(2, 12, "hr");
 ```
 
 ##### Request name day in country calendar
 
-Will return all days in given calendar which contains the name.
-
-> searchByName(name: string, countryCode: string)
-
-Both parameters are required. Parameter `countryCode` must be one of the supported [country codes](https://api.abalin.net/documentation)
-
 ```javascript
-nameday.searchByName("Jana", "cz") # {"calendar":"cz","results":[{"day":24,"month":5,"name":"Jana"},{"day":24,"month":6,"name":"Jan"} ... }}
+nameday.searchByName("Jana", "cz");
 ```
--- index.ts
+++ index.ts
@@ -1,16 +1,45 @@
+import fetch from "isomorphic-fetch";
+
 export type CountryCode =
-  | "us"
-  | "cz"
-  | "sk"
-  | "pl"
-  | "fr"
-  | "hu"
-  | "hr"
-  | "se"
   | "at"
+  | "dk"
+  | "fr"
   | "it"
+  | "sk"
+  | "cz"
+  | "es"
+  | "hr"
+  | "pl"
+  | "us"
   | "de"
-  | "es";
+  | "fi"
+  | "hu"
+  | "se";
+
+export type TimeZone =
+  | "America/Denver"
+  | "America/Costa_Rica"
+  | "America/Los_Angeles"
+  | "America/St_Vincent"
+  | "America/Toronto"
+  | "Europe/Amsterdam"
+  | "Europe/Monaco"
+  | "Europe/Prague"
+  | "Europe/Isle_of_Man"
+  | "Africa/Cairo"
+  | "Africa/Johannesburg"
+  | "Africa/Nairobi"
+  | "Asia/Yakutsk"
+  | "Asia/Hong_Kong"
+  | "Asia/Taipei"
+  | "Pacific/Midway"
+  | "Pacific/Honolulu"
+  | "Etc/GMT-6"
+  | "US/Samoa"
+  | "Zulu"
+  | "US/Hawaii"
+  | "Israel"
+  | "Etc/GMT-2";
 
 export type Day =
   | 1
@@ -48,89 +77,109 @@ export type Day =
 export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export interface Response {
-  day: number;
-  month: number;
-  name_us?: string;
-  name_cz?: string;
-  name_sk?: string;
-  name_fr?: string;
-  name_hu?: string;
-  name_hr?: string;
-  name_se?: string;
-  name_at?: string;
-  name_it?: string;
-  name_de?: string;
-  name_es?: string;
-  name_pl?: string;
+  dates: { day: Day; month: Month };
+  namedays: {
+    at?: string;
+    dk?: string;
+    fr?: string;
+    it?: string;
+    sk?: string;
+    cz?: string;
+    es?: string;
+    hr?: string;
+    pl?: string;
+    us?: string;
+    de?: string;
+    fi?: string;
+    hu?: string;
+    se?: string;
+  };
 }
 
 export interface ResponseSpecific {
-  calendar: string;
-  results: { day: number; month: number; name: string }[];
+  "country name": string;
+  "country code": CountryCode;
+  results: { day: Day; month: Month; name: string }[];
 }
 
-const today = async (countryCode?: CountryCode): Promise<Response> => {
-  if (countryCode) {
-    return fetch(
-      `https://api.abalin.net/get/today?country=${countryCode}`
-    ).then(res => res.json().then(data => data.data));
+const today = async (countryCode?: CountryCode, timeZone?: TimeZone): Promise<Response> => {
+  if (countryCode && timeZone) {
+    return fetch(`https://api.abalin.net/today?timezone=${timeZone}&country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (countryCode) {
+    return fetch(`https://api.abalin.net/today?country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (timeZone) {
+    return fetch(`https://api.abalin.net/today?timezone=${timeZone}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   } else {
-    return fetch("https://api.abalin.net/get/today").then(res =>
-      res.json().then(data => data.data)
-    );
+    return fetch("https://api.abalin.net/today", {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   }
 };
 
-const tomorrow = (countryCode?: CountryCode): Promise<Response> => {
-  if (countryCode) {
-    return fetch(
-      `https://api.abalin.net/get/tomorrow?country=${countryCode}`
-    ).then(res => res.json().then(data => data.data));
+const tomorrow = (countryCode?: CountryCode, timeZone?: TimeZone): Promise<Response> => {
+  if (countryCode && timeZone) {
+    return fetch(`https://api.abalin.net/tomorrow?timezone=${timeZone}&country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (countryCode) {
+    return fetch(`https://api.abalin.net/tomorrow?country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (timeZone) {
+    return fetch(`https://api.abalin.net/tomorrow?timezone=${timeZone}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   } else {
-    return fetch("https://api.abalin.net/get/tomorrow").then(res =>
-      res.json().then(data => data.data)
-    );
+    return fetch("https://api.abalin.net/tomorrow", {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   }
 };
 
-const yesterday = (countryCode?: CountryCode): Promise<Response> => {
-  if (countryCode) {
-    return fetch(
-      `https://api.abalin.net/get/yesterday?country=${countryCode}`
-    ).then(res => res.json().then(data => data.data));
+const yesterday = (countryCode?: CountryCode, timeZone?: TimeZone): Promise<Response> => {
+  if (countryCode && timeZone) {
+    return fetch(`https://api.abalin.net/yesterday?timezone=${timeZone}&country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (countryCode) {
+    return fetch(`https://api.abalin.net/yesterday?country=${countryCode}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
+  } else if (timeZone) {
+    return fetch(`https://api.abalin.net/yesterday?timezone=${timeZone}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   } else {
-    return fetch("https://api.abalin.net/get/yesterday").then(res =>
-      res.json().then(data => data.data)
-    );
+    return fetch("https://api.abalin.net/yesterday", {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   }
 };
 
-const specificDay = (
-  day: Day,
-  month: Month,
-  countryCode: CountryCode
-): Promise<Response> => {
+const specificDay = (day: Day, month: Month, countryCode: CountryCode): Promise<Response> => {
   if (countryCode) {
     return fetch(
-      `https://api.abalin.net/get/namedays?day=${day}&month=${month}&country=${countryCode}`
-    ).then(res => res.json().then(data => data.data));
+      `https://api.abalin.net/namedays?day=${day}&month=${month}&country=${countryCode}`,
+      { cache: "no-store" }
+    ).then(res => res.json().then(data => data.data[0]));
   } else {
-    return fetch(
-      `https://api.abalin.net/get/namedays?day=${day}&month=${month}`
-    ).then(res => res.json().then(data => data.data));
+    return fetch(`https://api.abalin.net/namedays?day=${day}&month=${month}`, {
+      cache: "no-store",
+    }).then(res => res.json().then(data => data.data[0]));
   }
 };
 
-const searchByName = (
-  name: string,
-  countryCode: CountryCode
-): Promise<ResponseSpecific> => {
-  return fetch(
-    `https://api.abalin.net/get/getdate?name=${name}&calendar=${countryCode}`
-  ).then(res => res.json().then(data => data));
+const searchByName = (name: string, countryCode: CountryCode): Promise<ResponseSpecific> => {
+  return fetch(`https://api.abalin.net/getdate?name=${name}&country=${countryCode}`, {
+    cache: "no-store",
+  }).then(res => res.json().then(data => data));
 };
-
-today("cz").then(res => console.log(res));
 
 const nameday = { today, tomorrow, yesterday, specificDay, searchByName };
 
